a5630760ebf953249116d748cb69055245a3438a
Allan Gong <hgon23@gmail.com>
2012-03-28 19:40:53 +1100

    Signed-off-by: Allan Gong <hgon23@gmail.com> rewrite access control rules

--- wp-content/themes/andloft/cron/cron.admin.weibo.php
+++ wp-content/themes/andloft/cron/cron.admin.weibo.php
@@ -2,10 +2,22 @@
 
 // prevent cron script to be accessed from outside this server
 
-$whitelist = array('localhost', '127.0.0.1');
+$LOCALHOSTS   = array('localhost', '127.0.0.1');
 
-if(!in_array($_SERVER['HTTP_HOST'], $whitelist)){
-    // not valid
+$HTTP_HOSTS   = array('www.loft.andshop.com.au');
+$REMOTE_ADDRS = array('110.232.142.51');
+$SERVER_ADDRS = array('110.232.142.51');
+
+$can_execute_cron_job = (
+	   in_array($_SERVER['HTTP_HOST'], $LOCALHOSTS)
+	or  ( 
+		    	in_array($_SERVER['REMOTE_ADDR'], $REMOTE_ADDRS) 
+			and in_array($_SERVER['SERVER_ADDR'], $SERVER_ADDRS)
+			and in_array($_SERVER['HTTP_HOST'],   $HTTP_HOSTS)
+		)
+);
+
+if( !$can_execute_cron_job ) {
     die('Permission denied.'); 
 }
 
